61c46cb08afefa0e65375cce9af75124950d8d08
Mateus Borja <mateus.borja@gmail.com>
2020-12-09 23:23:00 -0400

    biblioteca e modulos do datetime

--- py/02 - python2/date_time.py
+++ py/02 - python2/date_time.py
@@ -2,6 +2,15 @@
 #  Mateus Borja // UX Designer • 3D Artist • Developer
 #  www.mateusborja.life
 
+from datetime import date
 import datetime
 
-print(datetime.date.today().year)
+print(datetime.date.today().day)
+
+if datetime.date.today().day == 9:
+    print("hoje e o seu dia! Dia", date.today().day)
+else:
+    print("hoje nao e seu dia!")
+
+print(date.isoformat(date.today()))
+
