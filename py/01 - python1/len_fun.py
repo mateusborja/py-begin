40eac399b1abdaa3625159b4c4f35f71257868b2
Mateus Borja <mateus.borja@gmail.com>
2020-12-10 20:48:20 -0400

    funcao len

---
+++ py/01 - python1/len_fun.py
@@ -0,0 +1,8 @@
+#  Copyright (c) 2020.  | All rights reserved
+#  Mateus Borja // UX Designer • 3D Artist • Developer
+#  www.mateusborja.life
+
+frase = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+
+print('Caracteres:', len(frase))
+
