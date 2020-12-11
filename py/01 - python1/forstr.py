e8080a2ca8834299acff200a6c6d56a6537749c1
Mateus Borja <mateus.borja@gmail.com>
2020-12-10 20:47:24 -0400

    percorrendo strings

---
+++ py/01 - python1/forstr.py
@@ -0,0 +1,10 @@
+#  Copyright (c) 2020.  | All rights reserved
+#  Mateus Borja // UX Designer • 3D Artist • Developer
+#  www.mateusborja.life
+
+
+frase = 'Auditoria Interna - Painel de Auditoria - Auditoria em Andamento'
+ct = 0
+for i in frase:
+    ct+= 1
+    print(ct)
