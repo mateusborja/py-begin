df8c5ee5b67d98ae23162c41f0967da8c6b624af
Mateus Borja <mateus.borja@gmail.com>
2020-12-13 19:15:56 -0400

    for concatenado

---
+++ py/01 - python1/fornum.py
@@ -0,0 +1,12 @@
+#  Copyright (c) 2020.  | All rights reserved
+#  Mateus Borja // UX Designer • 3D Artist • Developer
+#  www.mateusborja.life
+
+
+num = [1,2,3,4,5,6,7,8,9]
+
+for i in num * 10:
+    print(i % 2, end="")
+
+
+
---
+++ py/01 - python1/patternletters.py
@@ -0,0 +1,10 @@
+#  Copyright (c) 2020.  | All rights reserved
+#  Mateus Borja // UX Designer • 3D Artist • Developer
+#  www.mateusborja.life
+
+for x in range(65,70):
+    for y in range(1, 6):
+        print(chr(x) , end="")
+    print()
+
+
