b8e9e9dba3e121e0b9d2d647343c1dc0dc2d5e77
Mateus Borja <mateus.borja@gmail.com>
2020-12-14 17:20:51 -0400

    exemplo de funcoes

---
+++ py/01 - python1/functionex01.py
@@ -0,0 +1,12 @@
+#  Copyright (c) 2020.  | All rights reserved
+#  Mateus Borja // UX Designer • 3D Artist • Developer
+#  www.mateusborja.life
+
+def func(v, i):
+    valor = v
+    incremento = i
+    resultado = valor + incremento
+    return resultado
+
+
+print("soma e:",func(4, 5))
